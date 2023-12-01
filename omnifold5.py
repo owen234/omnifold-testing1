5611d2000f96595fd93d9e71df3a94b032f88a0c
Owen Long <owen234@gmail.com>
2023-12-01 08:39:46 -0800

    update

---
+++ omnifold5.py
@@ -0,0 +1,187 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from sklearn.model_selection import train_test_split
+
+def reweight(events,model,batch_size=10000):
+    f = model.predict(events, batch_size=batch_size)
+    epsilon = K.epsilon()
+    f = K.clip( f, epsilon, 1. - epsilon )  #owen: protect against divide by zero.
+    weights = f / (1. - f)
+    return np.squeeze(np.nan_to_num(weights))
+
+# Binary crossentropy for classifying two samples with weights
+# Weights are "hidden" by zipping in y_true (the labels)
+
+def weighted_binary_crossentropy(y_true, y_pred):
+    weights = tf.gather(y_true, [1], axis=1) # event weights
+    y_true = tf.gather(y_true, [0], axis=1) # actual y_true for loss
+    
+    # Clip the prediction value to prevent NaN's and Inf's
+    epsilon = K.epsilon()
+    y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+
+    t_loss = -weights * ((y_true) * K.log(y_pred) +
+                         (1 - y_true) * K.log(1 - y_pred))
+    
+    return K.mean(t_loss)
+
+
+
+
+
+
+def omnifold5(theta0,theta_unknown_S,iterations,model,verbose=0,batch_size_setval=10000, learning_rate_setval=0.001, epochs_setval=20):
+
+    weights = np.empty(shape=(iterations, 2, len(theta0)))
+    # shape = (iteration, step, event)
+    push_weights_for_output = np.empty(shape=(iterations, len(theta0)))
+
+    theta0_G = theta0[:,0]
+    theta0_S = theta0[:,1]
+
+    labels0 = np.zeros(len(theta0))
+    labels_unknown = np.ones(len(theta_unknown_S))
+    labels_unknown_step2 = np.ones(len(theta0_G))
+
+    xvals_1 = np.concatenate((theta0_S, theta_unknown_S))
+    yvals_1 = np.concatenate((labels0, labels_unknown))
+
+    xvals_2 = np.concatenate((theta0_G, theta0_G))
+    yvals_2 = np.concatenate((labels0, labels_unknown_step2))
+
+    if verbose :
+        print("\n\n")
+        print("  ======== omnifold5\n\n")
+        print("  shape of theta0_S : %s" % str(np.shape(theta0_S)) ) ;
+        print("  shape of theta_unknown_S : %s" % str(np.shape(theta_unknown_S)) ) ;
+        print("  shape of xvals_1 :  %s" % str(np.shape( xvals_1 )) ) ;
+        print("\n\n")
+        print("  shape of labels0 : %s" % str(np.shape(labels0)) ) ;
+        print("  shape of labels_unknown : %s" % str(np.shape(labels_unknown)) ) ;
+        print("  shape of yvals_1 :  %s" % str(np.shape( yvals_1 )) ) ;
+        print("\n\n")
+        print("  shape of theta0_G : %s" % str(np.shape(theta0_G)) ) ;
+        print("  shape of xvals_2 :  %s" % str(np.shape( xvals_2 )) ) ;
+        print("  shape of yvals_2 :  %s" % str(np.shape( yvals_2 )) ) ;
+        print("\n batch size setval  %d" % batch_size_setval )
+        print(" learning rate setval  %f" % learning_rate_setval )
+        print(" epochs setval  %d" % epochs_setval )
+        print("\n\n")
+
+    # initial iterative weights are ones
+    weights_pull = np.ones(len(theta0_S))
+    weights_push = np.ones(len(theta0_S))
+
+
+    # owen: rescale the MC weights to balance the two categories for step 1 in first iteration.
+    mc_weight_sf = (1.*len(theta_unknown_S))/(1.*len(theta0_S))
+
+
+    return_dict = {}
+
+    for i in range(iterations):
+
+        if (verbose>0):
+            print("\nITERATION: {}\n".format(i + 1))
+            pass
+
+        # STEP 1: classify Sim. (which is reweighted by weights_push) to Data
+        # weights reweighted Sim. --> Data
+
+        if (verbose>0):
+            print("   -- ITERATION %d  STEP 1\n" % (i+1) )
+            pass
+
+        print(" weights_push at the beginning")
+        print( weights_push )
+
+     # owen: put the scaling here so that weights_push keeps its meaning (order 1).
+        weights_1 = np.concatenate((mc_weight_sf * weights_push, np.ones(len(theta_unknown_S))))
+
+        X_train_1, X_test_1, Y_train_1, Y_test_1, w_train_1, w_test_1 = train_test_split(xvals_1, yvals_1, weights_1)
+
+        # zip ("hide") the weights with the labels
+        Y_train_1 = np.stack((Y_train_1, w_train_1), axis=1)
+        Y_test_1 = np.stack((Y_test_1, w_test_1), axis=1)   
+
+        opt_step1 = tf.keras.optimizers.Adam( learning_rate=learning_rate_setval )
+
+        model.compile(loss=weighted_binary_crossentropy,
+                      optimizer=opt_step1,
+                      metrics=['accuracy'])
+
+        train_history_step1 = model.fit(X_train_1,
+                  Y_train_1,
+                  epochs=epochs_setval,
+                  batch_size=batch_size_setval,
+                  validation_data=(X_test_1, Y_test_1),
+                  verbose=verbose)
+
+        step1_output_weights = reweight(theta0_S,model)
+
+        weights_pull = weights_push * step1_output_weights
+
+        weights[i, :1, :] = step1_output_weights
+
+        return_dict["train-hist-step1-iter%d" % i] = train_history_step1
+
+
+
+
+        # STEP 2: classify Gen. to reweighted Gen. (which is reweighted by weights_pull)
+        # weights Gen. --> reweighted Gen.
+
+        if (verbose>0):
+            print("\n   -- ITERATION %d  STEP 2\n" % (i+1) )
+            pass
+
+        # ones for Gen. (not MC weights), actual weights for (reweighted) Gen.
+        ##########weights_2 = np.concatenate((   np.ones(len(theta0_G))  , weights_pull))
+        weights_2 = np.concatenate((   weights_push  , weights_pull))
+
+        X_train_2, X_test_2, Y_train_2, Y_test_2, w_train_2, w_test_2 = train_test_split(xvals_2, yvals_2, weights_2)
+
+        # zip ("hide") the weights with the labels
+        Y_train_2 = np.stack((Y_train_2, w_train_2), axis=1)
+        Y_test_2 = np.stack((Y_test_2, w_test_2), axis=1)   
+
+        opt_step2 = tf.keras.optimizers.Adam( learning_rate=learning_rate_setval )
+
+        model.compile(loss=weighted_binary_crossentropy,
+                      optimizer=opt_step2,
+                      metrics=['accuracy'])
+
+        train_history_step2 = model.fit(X_train_2,
+                  Y_train_2,
+                  epochs=epochs_setval,
+                  batch_size=batch_size_setval,
+                  validation_data=(X_test_2, Y_test_2),
+                  verbose=verbose)
+
+        return_dict["train-hist-step2-iter%d" % i] = train_history_step2
+
+        step2_output_weights = reweight(theta0_G,model)
+
+        weights_push = step2_output_weights * weights_push
+
+        push_weights_for_output[i] = weights_push
+
+        weights[i, 1:2, :] = step2_output_weights
+
+
+        pass
+
+
+    return_dict["weights"] = weights
+
+    return_dict["push_weights"] = push_weights_for_output
+
+    return_dict["final_push_weights"] = weights_push
+
+    return return_dict
+
+
+
+
+
